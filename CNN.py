1dd6f9ac6fd4ee289c5c40eb8c63e81300562272
Yijuan Liang <yjliang@mail.ustc.edu.cn>
2026-01-18 11:54:41 +0800

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -6,24 +6,24 @@ import torchvision.models as models
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-# 定义图像增强的transforms
+
 transform = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-# 加载训练数据集
+
 train_dataset = ImageFolder('图像训练集', transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-# 加载预训练的resnet18模型
+
 model = models.resnet18(pretrained=True)
 num_classes = 2
 model.fc = nn.Linear(512, num_classes)
-# 定义损失函数和优化器
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-# 训练模型
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 for epoch in range(50):
@@ -38,6 +38,7 @@ for epoch in range(50):
         optimizer.step()
         running_loss += loss.item()
     print(f"Epoch {epoch + 1}: Loss {running_loss / len(train_loader)}")
-# 保存模型
+
 save_path = 'resnet18_model.pth'
+
 torch.save(model.state_dict(), save_path)
